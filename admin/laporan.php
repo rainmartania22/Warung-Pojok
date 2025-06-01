464e9c4ace81113e5c30c48537d70c5aea40429e
rainmartaniamarrosuli <rainmartaniamrosuli@gmail.com>
2025-06-01 11:27:05 +0700

    memperbaiki isi laporan.php

--- admin/laporan.php
+++ admin/laporan.php
@@ -25,7 +25,7 @@ if (!isset($_SESSION["status"]) || $_SESSION["status"] !== "admin") {
     <meta charset="utf-8">
     <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
-    <title>Laporan - PantryNusantara Admin</title>
+    <title>Laporan - WarungPojok Admin</title>
     <meta content="" name="description">
     <meta content="" name="keywords">
 
@@ -59,7 +59,7 @@ if (!isset($_SESSION["status"]) || $_SESSION["status"] !== "admin") {
         <div class="d-flex align-items-center justify-content-between">
             <a href="index.php" class="logo d-flex align-items-center">
                 <img src="assets/img/logo.png" alt="">
-                <span class="d-none d-lg-block">PantryNusantara</span>
+                <span class="d-none d-lg-block">WarungPojok</span>
             </a>
             <i class="bi bi-list toggle-sidebar-btn"></i>
         </div><!-- End Logo -->
